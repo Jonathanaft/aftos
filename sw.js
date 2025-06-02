8442fc9ce64f877ed08a17cd60fe46c3586d7aee
Jonathanaft <152176072+Jonathanaft@users.noreply.github.com>
2025-06-02 14:28:53 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'reaftos-cache-v1';
+const urlsToCache = [
+  '/',
+  '/aftos_/',
+  // Add other assets you want to cache for offline use
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened reaftos cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
